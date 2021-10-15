121f965fadabc3d4c7382fdab41d22606886a392
Rodrigo Toledo <rodrigo@rtoledo.inf.br>
2021-10-15 19:39:38 -0300

    - validates url model - validates click model

--- spec/models/url_spec.rb
+++ spec/models/url_spec.rb
@@ -3,15 +3,19 @@
 require 'rails_helper'
 
 RSpec.describe Url, type: :model do
+  describe 'associations' do
+    it { should have_many(:clicks).class_name('Click') }
+  end
+
   describe 'validations' do
-    it 'validates original URL is a valid URL' do
-      skip 'add test'
+    before do
+      create(:url, original_url: 'http://www.teste.com')
     end
 
-    it 'validates short URL is present' do
-      skip 'add test'
+    it { is_expected.not_to validate_presence_of(:original_url).allow_nil }
+    it 'should be uniqueness' do
+      url = build(:url, original_url: 'http://www.teste.com')
+      url.should validate_uniqueness_of(:original_url)
     end
-
-    # add more tests
   end
 end
